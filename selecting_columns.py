c6765c7a3ec8289ebf4f4da026ef68970c7e59a4
Cem Baykal <cbaykal@unc.edu>
2024-04-24 01:01:19 -0400

    Cleaning up files

---
+++ selecting_columns.py
@@ -0,0 +1,12 @@
+import pandas as pd
+initial_df = pd.read_csv('Collisions.csv') # All attributes
+# Declaring needed columns
+df = pd.DataFrame()
+wanted_columns = ['X', 'Y', 'ADDRTYPE','SEVERITYCODE', 'VEHCOUNT', 'INJURIES', 'SERIOUSINJURIES',
+                 'FATALITIES', 'JUNCTIONTYPE', 'UNDERINFL', 'WEATHER', 'ROADCOND', 'LIGHTCOND', 'ST_COLCODE']
+# Constructing new dataset with only those necessary
+for c in wanted_columns:
+        df[str(c)] = initial_df[c]
+
+
+df.to_csv("SelectedCollisionCols.csv", index=False)
