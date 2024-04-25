1ee3ef89682ca3e250d4bcd8d4d1c96f536b933f
Cem Baykal <cbaykal@unc.edu>
2024-04-25 11:03:41 -0400

    New collision columns

--- selecting_columns.py
+++ selecting_columns.py
@@ -3,7 +3,7 @@ initial_df = pd.read_csv('Collisions.csv') # All attributes
 # Declaring needed columns
 df = pd.DataFrame()
 wanted_columns = ['X', 'Y', 'ADDRTYPE','SEVERITYCODE', 'VEHCOUNT', 'INJURIES', 'SERIOUSINJURIES',
-                 'FATALITIES', 'JUNCTIONTYPE', 'UNDERINFL', 'WEATHER', 'ROADCOND', 'LIGHTCOND', 'ST_COLCODE']
+                 'FATALITIES', 'JUNCTIONTYPE', 'UNDERINFL', 'WEATHER', 'ROADCOND', 'LIGHTCOND']
 # Constructing new dataset with only those necessary
 for c in wanted_columns:
         df[str(c)] = initial_df[c]
